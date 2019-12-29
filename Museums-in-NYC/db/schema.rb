222f674c338227ef754b74d487804c98431fa6a8
GAierken <g.aierken@gmail.com>
2019-12-28 22:49:46 -0500

    user and review are generated, relationships between user, review, museum have been established

--- Museums-in-NYC/db/schema.rb
+++ Museums-in-NYC/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_20_222027) do
+ActiveRecord::Schema.define(version: 2019_12_29_034120) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,4 +26,23 @@ ActiveRecord::Schema.define(version: 2019_12_20_222027) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "content"
+    t.bigint "user_id", null: false
+    t.bigint "museum_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["museum_id"], name: "index_reviews_on_museum_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "reviews", "museums"
+  add_foreign_key "reviews", "users"
 end
--- Museums-in-NYC/db/seeds.rb
+++ Museums-in-NYC/db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Museum.destroy_all
+User.destroy_all
+### Museums######
 met = Museum.create(name: "The Metropolitan Museum of Art", address: "1000 5th Ave, New York, NY 10028", description: "The Met presents over 5,000 years of art from around the world for everyone to experience and enjoy. The Museum lives in three iconic sites in New York City—The Met Fifth Avenue, The Met Breuer, and The Met Cloisters. Millions of people also take part in The Met experience online. Since it was founded in 1870, The Met has always aspired to be more than a treasury of rare and beautiful objects. Every day, art comes alive in the Museum's galleries and through its exhibitions and events, revealing both new ideas and unexpected connections across time and across cultures.", image_url: "https://www.metmuseum.org/-/media/images/visit/plan-your-visit/individual-locations/fifth-avenue/fifthave_1520x1520.jpg?as=1&la=en&mh=3040&mw=3040&hash=B1EA90DB4676B82D60470BBEDD6458F9", hours:"Sunday – Thursday: 10:00 am – 5:30 pm
 Friday and Saturday: 10:00 am – 9:00 pm
 Open Seven Days a Week", homepage:"https://www.metmuseum.org/")
@@ -116,3 +118,15 @@ Fri - Wed
 10:00 AM - 6:30 PM", homepage: "https://www.tenement.org/")
 
 # Museum.create(name: "", address: "", description: "", image_url: "", hours: "", homepage: "")
+
+
+###### Users
+u1 = User.create(name: "Gena", email: "gena@163.com")
+u2 = User.create(name: "Kaiser", email: "kayser@126.com")
+
+
+##### Review
+Review.create(content: "Amazing experience!", user: u1, museum: met)
+Review.create(content: "Special experience!", user: u1, museum: moma)
+Review.create(content: "Cool!", user: u2, museum: met)
+Review.create(content: "Deep!", user: u2, museum: moma)
